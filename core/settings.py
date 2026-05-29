26bf807e75219ce4680c214a08d0f97839a243ca
Sreeram P <psreeram3015@proton.me>
2026-05-29 15:11:20 +0530

    configure allowed hosts for production

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=False, cast=bool)
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config(
+    "ALLOWED_HOSTS",
+    default="localhost,127.0.0.1",
+    cast=lambda v: [s.strip() for s in v.split(",")],
+)
 
 
 # Application definition
